5b96c840d864e0282e8f7c1a6e3d0a22ec673714
Charlie Furniss <charliefurniss96@gmail.com>
2016-08-13 10:54:02 +0100

    adding basic html structure, TF logo and copy

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
 	console.log('the dom is ready');
+	
+	// function cb(data) {
+	// 	//use returned data
+ //    }
+    
+ //    (function () {
+ //        var tags = 'london';
+ //        var script = document.createElement('script');
+ //        script.src =
+ //        'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=cb&tags='
+ //        + tags;
+ //        document.head.appendChild(script);
+ //    })();
 
 });
